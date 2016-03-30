45267c348320b2cd8af7728200e6c54530aebd1d
Marais Rossouw <me@maraisr.com>
2016-03-30 15:36:31 +1100

    chore: starting of the ast

--- dist/Main.js
+++ dist/Main.js
@@ -1,5 +1,6 @@
 /// <reference path="../typings/main.d.ts" />
 "use strict";
+var parser = require('acorn');
 var Views_1 = require('./Views');
 var Templicated = (function () {
     function Templicated(source, config) {
@@ -7,6 +8,20 @@ var Templicated = (function () {
         this.config = config;
         this.views = new Views_1.default(this.config);
     }
+    Templicated.prototype.compile = function () {
+        var parsed = new Parse(this.source);
+        console.log(parsed.findTpls());
+        return new Buffer('');
+    };
     return Templicated;
 }());
 exports.Templicated = Templicated;
+var Parse = (function () {
+    function Parse(source) {
+        this.parsed = parser.parse(source.toString('utf-8')).body;
+    }
+    Parse.prototype.findTpls = function () {
+        return new Array();
+    };
+    return Parse;
+}());
--- src/Main.ts
+++ src/Main.ts
@@ -1,6 +1,9 @@
 /// <reference path="../typings/main.d.ts" />
 
 import fs = require('graceful-fs');
+import parser = require('acorn');
+
+import util = require('util');
 
 import {Config} from './Common';
 
@@ -16,5 +19,26 @@ export class Templicated {
 		this.config = config;
 
 		this.views = new Views(this.config);
+	}
+
+	private compile(): Buffer {
+		let parsed = new Parse(this.source);
+
+		console.log(parsed.findTpls());
+
+		return new Buffer('');
+	}
+}
+
+
+class Parse {
+	private parsed: any;
+
+	constructor(source: Buffer) {
+		this.parsed = parser.parse(source.toString('utf-8')).body;
+	}
+
+	public findTpls(): Array<string> {
+		return new Array();
 	}
 }
