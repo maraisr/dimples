5f57001f4d1e176b56c2180cf310499497edef4b
Marais Rossouw <me@maraisr.com>
2016-03-30 12:30:04 +1100

    chore: basic building of jade templates

--- dist/Main.js
+++ dist/Main.js
@@ -1,7 +1,30 @@
 "use strict";
+var jade = require('jade');
+var fs = require('graceful-fs');
+var viewUid = -1;
 var Templicated = (function () {
-    function Templicated() {
+    function Templicated(source, config) {
+        this.source = source;
+        this.config = config;
+        this.views = this.getViews();
     }
+    Templicated.prototype.getViews = function () {
+        var _this = this;
+        var returns = new Array();
+        fs.readdirSync(this.config.views).forEach(function (path) {
+            returns.push(new View(path, _this.config));
+        });
+        return returns;
+    };
     return Templicated;
 }());
 exports.Templicated = Templicated;
+var View = (function () {
+    function View(path, config) {
+        this.path = path;
+        this.uid = viewUid += 1;
+        this.compiled = jade.compile(fs.readFileSync(config.views + this.path, 'utf-8'))();
+        this.name = this.path.replace(/\.jade/, '').trim();
+    }
+    return View;
+}());
--- src/Main.ts
+++ src/Main.ts
@@ -1,9 +1,52 @@
 /// <reference path="../typings/main.d.ts" />
 
 import jade = require('jade');
+import fs = require('graceful-fs');
+
+let viewUid = -1;
+
+interface Config {
+	views: string;
+}
 
 export class Templicated {
-	constructor() {
+	private source: Buffer;
+	private config: Config;
+	public views: Array<View>;
 
+	constructor(source: Buffer, config: Config) {
+		this.source = source;
+		this.config = config;
+
+		this.views = this.getViews();
+	}
+
+	private getViews(): Array<View> {
+
+		let returns = new Array();
+
+		fs.readdirSync(this.config.views).forEach((path) => {
+			returns.push(new View(path, this.config));
+		})
+
+		return returns;
+	}
+}
+
+class View {
+	private path: string;
+
+	public compiled: string;
+	public name:string;
+
+	public uid:number;
+
+	constructor(path: string, config: Config) {
+		this.path = path;
+		this.uid = viewUid += 1;
+
+		this.compiled = jade.compile(fs.readFileSync(config.views + this.path, 'utf-8'))();
+
+		this.name = this.path.replace(/\.jade/, '').trim();
 	}
 }
